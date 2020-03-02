abc0c893b002825d681040f2c1a01bfc8ceab43c
ranjith909 <ranjithrao909@gmail.com>
2020-03-02 19:52:18 +0530

    add

---
+++ B.java
@@ -0,0 +1,10 @@
+public class B{
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A devs=new A();
+	
+                    System.out.println(devs.method());
+	}
+
+}
